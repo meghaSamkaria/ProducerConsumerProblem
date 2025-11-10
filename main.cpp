760649da08401e335f3901364422aa1014f2a187
meghaSamkaria <megha.samkaria@gmail.com>
2025-11-10 15:14:12 +0530

    producer consumer problem with queue

--- main.cpp
+++ main.cpp
@@ -5,33 +5,63 @@
 #include <iostream>
 #include <condition_variable>
 #include <chrono>
+#include <queue>
 
 
 int products{0};
-int capacity{50};
+int capacity{10};
+
+//std::condition_variable cv_produce;
+//std::condition_variable cv_consume;
 std::condition_variable cv;
 std::mutex mtx;
+bool readyToConsumeFlag = false;
+bool readyToProduceFlag = true;
+
+class ServingLine {
+public:
+    std::queue<int> q;
+public :
+    void fillServingLine() {
+        q.push(1);
+    }
+    void removeFromServingLine() {
+        q.pop();
+    }
+};
+
+ServingLine ourLine;
 
 void produce() {
     while(products < capacity) {
         std::unique_lock<std::mutex> lock(mtx);
+        cv.wait(lock, []{ return readyToProduceFlag;});
         products++;
-        std::this_thread::sleep_for(std::chrono::milliseconds(500)); // Simulate work
+        ourLine.fillServingLine();
+
         std::cout<<"produced products = "<<products<<std::endl;
-        if(products == 10) {
+        if(!ourLine.q.empty()) {
+            lock.unlock(); //this line is important, unlock the mutex for consumer to proceed
             cv.notify_one();
-            break;
         }
+        std::this_thread::sleep_for(std::chrono::milliseconds(50)); // play around with this parameter
     }
 }
 
 void consume() {
-    std::unique_lock<std::mutex> lock(mtx);
-    cv.wait(lock, []{return products == 10;});
-    while(products > 0) {
-        products--;
-        std::this_thread::sleep_for(std::chrono::seconds(1)); // Simulate work
-        std::cout<<"consumed products = "<<products<<std::endl;
+    int consumed = 0;
+    while(consumed < capacity) {
+        std::unique_lock<std::mutex> lock(mtx);
+        cv.wait(lock, []{ return !ourLine.q.empty();});
+        consumed++;
+        ourLine.removeFromServingLine();
+
+        std::cout<<"consumed products = "<<consumed<<std::endl;
+        if(ourLine.q.empty()) {
+            lock.unlock();
+            cv.notify_one();
+        }
+        std::this_thread::sleep_for(std::chrono::milliseconds(250)); // Simulate work
     }
 }
 
